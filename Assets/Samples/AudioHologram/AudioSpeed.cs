70b3345fd22ab377967caddd8a74e4f22eaee3f7
pearsonkyle <pearsonk@email.arizona.edu>
2020-03-11 21:05:54 -0700

    v1

---
+++ Assets/Samples/AudioHologram/AudioSpeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+public class AudioSpeed : MonoBehaviour
+{
+    Animator m_Animator;
+    public LoopbackAudio Audio;
+    public float speed = 1f;  
+    public PlayableDirector playableDirector; 
+
+    void Start()
+    {
+        playableDirector = gameObject.GetComponent<PlayableDirector>();
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayableExtensions.SetSpeed(playableDirector.playableGraph.GetRootPlayable(0),( Audio.WeightedAverage) * speed ); 
+        //m_Animator.speed =;
+        //.SetSpeed();
+    }
+}
